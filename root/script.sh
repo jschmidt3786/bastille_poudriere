12ea5109fffc641de20f3c6e3b5739e8e8499ffd
Jeff Schmidt <jeff.schmidt@zenith.com>
2022-02-15 23:07:59 -0600

    bootstrap script, more notes

--- root/script.sh
+++ root/script.sh
@@ -1,15 +1,39 @@
 #!/bin/sh
 echo creating an RSA keypair for signing packages
+
+mkdir -p /usr/local/etc/ssl/certs
+mkdir -p /usr/local/etc/ssl/keys
+chmod 0600 /usr/local/etc/ssl/keys
+openssl genrsa -out /usr/local/etc/ssl/keys/poudriere.key 4096
+openssl rsa -in /usr/local/etc/ssl/keys/poudriere.key -pubout -out /usr/local/etc/ssl/certs/poudriere.cert
+
+echo changing:
+grep PKG_REPO_SIGNING_KEY /usr/local/etc/poudriere.conf
+sed -i '' 's:#PKG_REPO_SIGNING_KEY.*:PKG_REPO_SIGNING_KEY=/usr/local/etc/ssl/keys/poudriere.key:' /usr/local/etc/poudriere.conf
+echo
+echo to:
+grep PKG_REPO_SIGNING_KEY /usr/local/etc/poudriere.conf
+echo
+
+
 cd /usr/local/etc/poudriere.d || exit
 openssl genrsa -out repo.key 2048
 chmod 0400 repo.key
 openssl rsa -in repo.key -out repo.pub -pubout
 
 echo "TODO:"
-echo "  - self-signed keypair for nginx at /usr/local/etc/nginx/tls/"
+echo
+echo "  - run this: sed -i '' 's:txt;:txt log;:' /usr/local/etc/nginx/mime.types"
+echo "    to read logs rather than download them"
+echo
+echo "  - tls keypair for nginx at /usr/local/etc/nginx/tls/ is self-signed."
+echo "    don't leave it that way and be That Guy..."
+echo
+echo "  - and/change these in /bastille/jails/<jail>/jail.conf"
+echo "    note the hostname here is listed as 'poudriere'"
 echo
 echo "  enforce_statfs = 1;
-  exec.poststart = '/sbin/zfs jail packages zroot/data/poudriere';
+  exec.poststart = '/sbin/zfs jail poudriere virt/data/poudriere';
   securelevel = -1;
   allow.mount;
   allow.mount.devfs;
@@ -21,8 +45,8 @@ echo "  enforce_statfs = 1;
   children.max = 30;
 "
 
-echo "zfs create -o mountpoint=/data -o canmount=off zroot/data
-zfs create -o jailed=on -o mountpoint=/poudriere zroot/data/poudriere
-zfs jail packages zroot/data/poudriere # jail needs to be up - needs to be run each time jail is started
+echo "zfs create -o mountpoint=/data -o canmount=off virt/data
+zfs create -o jailed=on -o mountpoint=/poudriere virt/data/poudriere
+zfs jail poudriere virt/data/poudriere # jail needs to be up - needs to be run each time jail is started
 "
-
+echo "and edit /usr/local/etc/poudriere.conf"
--- usr/local/bin/sign.sh
+++ usr/local/bin/sign.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
+CERT=/usr/local/etc/ssl/certs/poudriere.cert
+KEY=/usr/local/etc/ssl/keys/poudriere.key
+# shellcheck disable=SC2162
+# shellcheck disable=SC3045
 read -t 2 sum
 [ -z "$sum" ] && exit 1
 echo SIGNATURE
-echo -n $sum | /usr/bin/openssl dgst -sign repo.key -sha256 -binary
+# shellcheck disable=SC3037
+# shellcheck disable=SC2086
+echo -n $sum | /usr/bin/openssl dgst -sign "$KEY" -sha256 -binary
 echo
 echo CERT
-cat repo.pub
+cat "$CERT"
 echo END
-
